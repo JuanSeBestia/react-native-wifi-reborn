e3a2cfc18f030827dd81b134f8458e1d88c8b7aa
JuanSe Dussan <Juan.Dussan@nordstrom.com>
2019-10-24 14:28:15 -0500

    rename folder example

--- example/AwesomeProject/index.js
+++ example/AwesomeProject/index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import {name as appName} from './app.json.js';
 
 AppRegistry.registerComponent(appName, () => App);
