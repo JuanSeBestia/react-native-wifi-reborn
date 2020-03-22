a905608624d8a784b64d4b9d4f48ce3b69bafef9
Elias Lecomte <elias.lecomte@inthepocket.com>
2020-03-22 08:31:49 +0100

    Add description on ForceWifiUsageErrorCode.

--- android/src/main/java/com/reactlibrary/rnwifi/ForceWifiUsageErrorCodes.java
+++ android/src/main/java/com/reactlibrary/rnwifi/ForceWifiUsageErrorCodes.java
@@ -1,5 +1,9 @@
 package com.reactlibrary.rnwifi;
 
 public enum ForceWifiUsageErrorCodes {
+    /**
+     * Could not get the ConnectivityManager.
+     * https://developer.android.com/reference/android/net/ConnectivityManager?hl=en
+     */
     couldNotGetConnectivityManager,
 }
