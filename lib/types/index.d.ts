bdfc48a7504a2844fc771f3bf96a74cb48de25af
Seph Soliman <scarlac@gmail.com>
2021-04-27 13:57:04 -0700

    Add return type for forceWifiUsageWithOptions
    Addresses part of #168

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -249,7 +249,7 @@ declare module 'react-native-wifi-reborn' {
      * @param options `noInternet` To indicate the access point has no internet. Usefull as some
      * phone vendor customizations will switch back to mobile when the wifi access point has no internet.
      */
-    export function forceWifiUsageWithOptions(useWifi: boolean, options: { noInternet: boolean });
+    export function forceWifiUsageWithOptions(useWifi: boolean, options: { noInternet: boolean }): Promise<void>;
 
     //#endregion
 }
