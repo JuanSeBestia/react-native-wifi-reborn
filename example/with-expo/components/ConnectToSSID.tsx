dfd96615e3a9694b634027970583dd1bed773222
robrechtme <robrechtmeersman@hotmail.com>
2023-06-09 20:34:31 +0200

    feat: add with-expo example

---
+++ example/with-expo/components/ConnectToSSID.tsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+import { Button, TextInput, StyleSheet, Text, ActivityIndicator } from 'react-native';
+import WifiManager from 'react-native-wifi-reborn';
+import { Section } from './Section';
+
+export const ConnectToSSID = () => {
+  const [ssid, setSsid] = useState('SSID');
+  const [pass, setPass] = useState('password');
+  const [error, setError] = useState('');
+  const [response, setResponse] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleConnect = () => {
+    setError('');
+    setResponse('');
+    setIsLoading(true);
+    WifiManager.connectToProtectedSSID(ssid, pass, false, false)
+      .then((r) => setResponse(JSON.stringify(r, null, 2)))
+      .catch((e) => setError(e.toString()))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <Section title="Connect to SSID">
+      <TextInput style={styles.textInput} value={ssid} onChangeText={setSsid} />
+      <TextInput style={styles.textInput} value={pass} onChangeText={setPass} />
+      <Button title="Connect" onPress={handleConnect} disabled={isLoading} />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <Text style={styles.error}>{error}</Text>
+          <Text>{response}</Text>
+        </>
+      )}
+    </Section>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 12,
+    backgroundColor: '#fff',
+    padding: 12,
+    borderRadius: 10,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#e5e5ea',
+    padding: 4,
+    borderRadius: 4,
+  },
+  error: {
+    color: 'red',
+  },
+});
---
+++ example/with-expo/components/CurrentSSID.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { Button, TextInput, View, StyleSheet, Text } from 'react-native';
+import WifiManager from 'react-native-wifi-reborn';
+import { Section } from './Section';
+
+export const CurrentSSID = () => {
+  const [ssid, setSsid] = useState('fetching...');
+
+  const handleFetch = () => {
+    setSsid('fetching...');
+    WifiManager.getCurrentWifiSSID()
+      .then(setSsid)
+      .catch((e) => setSsid(e.toString()));
+  };
+
+  useEffect(() => {
+    handleFetch;
+  }, []);
+
+  return (
+    <Section title="Current SSID">
+      <Text>{ssid}</Text>
+      <Button title="Refresh" onPress={handleFetch} />
+    </Section>
+  );
+};
